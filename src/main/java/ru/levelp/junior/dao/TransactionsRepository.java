7c1756378366f9722f680d3584c23606fe2cf4e5
Sergey Mashkov <cy6ergn0m@gmail.com>
2019-11-30 13:39:29 +0300

    Add rest repositories

--- src/main/java/ru/levelp/junior/dao/TransactionsRepository.java
+++ src/main/java/ru/levelp/junior/dao/TransactionsRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import ru.levelp.junior.entities.Account;
 import ru.levelp.junior.entities.Transaction;
 
 @Repository
+@RepositoryRestResource(collectionResourceRel = "transactions", path = "transactions")
 public interface TransactionsRepository extends PagingAndSortingRepository<Transaction, Long> {
 //    @Query("select a from Transaction a where a.origin = :account or a.receiver = :account")
 //    Page<Transaction> findByAccount(Account account,
@@ -19,4 +21,8 @@ public interface TransactionsRepository extends PagingAndSortingRepository<Trans
     @Query("select a from Transaction a where a.origin = :account or a.receiver = :account")
     Page<Transaction> findByAccount(@Param("account") Account account,
                                              Pageable pageable);
+
+    @Query("select a from Transaction a where a.origin.id = :accountId or a.receiver.id = :accountId")
+    Page<Transaction> findByAccountId(@Param("accountId") int accountId,
+                                    Pageable pageable);
 }
--- src/main/java/ru/levelp/junior/web/AppConfig.java
+++ src/main/java/ru/levelp/junior/web/AppConfig.java
@@ -3,12 +3,15 @@ package ru.levelp.junior.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 @Configuration
 @ComponentScan(basePackages = "ru.levelp.junior")
 @EnableJpaRepositories(basePackages = "ru.levelp.junior.dao")
+@Import(RepositoryRestMvcConfiguration.class)
 public class AppConfig {
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
